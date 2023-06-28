fe011a0faf2eebb0bf2707b8a9c57db815aed0d7
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-06-28 13:53:10 +0200

    homework task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,16 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  constructor() {
+    super();
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    this.state.contacts = parsedContacts || [];
+  }
 
   addNewContact = e => {
     e.preventDefault();
@@ -50,6 +52,10 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   render() {
     const { contacts, filter } = this.state;
